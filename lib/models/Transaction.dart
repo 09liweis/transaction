9fcbde4625f041cbd99164d858d1b4f9e2d475d9
Sam Li <weisen.li@hotmail.com>
2019-12-06 17:03:17 -0500

    add price to transaction model

--- lib/models/Transaction.dart
+++ lib/models/Transaction.dart
@@ -3,21 +3,24 @@ class Transaction {
   String title;
   String date;
   String category;
+  int price;
 
-  Transaction(String _id, String title, String date,String category) {
+  Transaction(String _id, int price, String title, String date,String category) {
     this._id = _id;
     this.title = title;
     this.date = date;
     this.category = category;
+    this.price = price;
   }
 
   Transaction.fromJson(Map json)
       : _id = json['_id'],
         title = json['title'],
         date = json['date'],
+        price = json['price'],
         category = json['category'];
 
   Map toJson() {
-    return {'_id': _id, 'title': title, 'date': date,'category':category};
+    return {'_id': _id, 'title': title, 'price':price, 'date': date,'category':category};
   }
 }
