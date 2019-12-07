892b98246554dec6bf36628ab1540a174bf39674
Sam Li <weisen.li@hotmail.com>
2019-12-06 23:10:31 -0500

    add float button to go to form view

--- lib/views/TransactionList.dart
+++ lib/views/TransactionList.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import '../API.dart';
 import '../models/Transaction.dart';
+import '../views/TransactionForm.dart';
 class MyListScreen extends StatefulWidget {
   @override
   createState() => _MyListScreenState();
@@ -18,6 +19,12 @@ class _MyListScreenState extends State {
       });
     });
   }
+  _gotoForm() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TransactionForm()),
+    );
+  }
 
   initState() {
     super.initState();
@@ -31,21 +38,29 @@ class _MyListScreenState extends State {
   @override
   build(context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Transaction List"),
-        ),
-        body: ListView.builder(
-          itemCount: transactions.length,
-          itemBuilder: (context, index) {
-            var t = transactions[index];
-            return ListTile(
-              leading: Text(t.category),
-              title: Text(t.title),
-              subtitle: Text(t.date),
-              trailing: Text(t.price.toString()),
-              enabled: true,
-            );
-          },
-        ));
+      appBar: AppBar(
+        title: Text("Transaction List"),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: transactions.length,
+        itemBuilder: (context, index) {
+          var t = transactions[index];
+          return ListTile(
+            leading: Text(t.category),
+            title: Text(t.title),
+            subtitle: Text(t.date),
+            trailing: Text(t.price.toString()),
+            enabled: true,
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _gotoForm,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+        
   }
 }
