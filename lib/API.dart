11011f28f9d87549261e3f99ec26e29e8a1f09f0
Sam Li <weisen.li@hotmail.com>
2019-12-08 11:14:05 -0500

    add post transaction

--- lib/API.dart
+++ lib/API.dart
@@ -1,12 +1,23 @@
 
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-const baseUrl = "http://samliweisen.herokuapp.com/api/";
+const baseUrl = "https://samliweisen.herokuapp.com/api/";
+// const baseUrl = "http://localhost:8081/api/";
 
 class API {
   static Future getTransactions() {
     var url = baseUrl + "transactions";
     return http.get(url);
+  }
+  static Future postTransaction(Map data) async {
+    var url = baseUrl + 'transactions';
+    var body = json.encode(data);
+    var response = await http.post(url,
+      headers: {"Content-Type": "application/json"},
+      body: body
+    );
+    return response;
   }
 }
