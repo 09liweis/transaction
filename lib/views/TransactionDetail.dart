d84c7d81c523807be0271d078dbe105b03d8d3ac
Sam Li <weisen.li@hotmail.com>
2019-12-10 16:10:05 -0500

    transaction detail accept data from list page

--- lib/views/TransactionDetail.dart
+++ lib/views/TransactionDetail.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
+import '../models/Transaction.dart';
 class TransactionDetail extends StatelessWidget {
+  final Transaction transaction;
+  TransactionDetail({Key key, @required this.transaction}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Transaction Detail'),
+        title: Text(transaction.title),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Detail'),
       ),
     );
   }
