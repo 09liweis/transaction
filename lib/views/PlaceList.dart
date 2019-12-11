770ccf391a07d3166f0d80eced11be9fc8a83c05
Sam Li <weisen.li@hotmail.com>
2019-12-11 15:11:57 -0500

    add place list page and api

--- lib/views/PlaceList.dart
+++ lib/views/PlaceList.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
-class PlaceList extends StatelessWidget {
+import 'dart:convert';
+import '../API.dart';
+import '../models/Place.dart';
+class PlaceList extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  createState() => _PlaceListState();
+}
+
+class _PlaceListState extends State {
+  var places = new List<Place>();
+
+  _getPlaces() {
+    API.getPlaces().then((response) {
+      setState(() {
+        Iterable list = json.decode(response.body);
+        places = list.map((model) => Place.fromJson(model)).toList();
+      });
+    });
+  }
+  _gotoDetail(t) {
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(
+    //     builder: (context) => PlaceDetail(transaction: t)
+    //   ),
+    // );
+  }
+  initState() {
+    super.initState();
+    _getPlaces();
+  }
+
+  dispose() {
+    super.dispose();
+  }
+
+  @override
+  build(context) {
     return Scaffold(
-      body: Center(
-        child:Text('Place list page')
-      ),
+      body: Stack(
+        children: <Widget>[
+          _googleMap(context)
+        ],
+      )
     );
   }
+}
+
+Widget _googleMap(BuildContext context) {
+  return Container(
+    height: MediaQuery.of(context).size.height,
+    width: MediaQuery.of(context).size.width,
+  );
 }
