a7ad3da85bf253fe3db6ad897fa2f763e7471e18
Sam Li <weisen.li@hotmail.com>
2019-12-13 11:13:47 -0500

    add placebox on map

--- lib/views/PlaceList.dart
+++ lib/views/PlaceList.dart
@@ -14,7 +14,6 @@ class _PlaceListState extends State {
   var places = new List<Place>();
   List<Marker> _markers = <Marker>[];
   Completer<GoogleMapController> _controller = Completer();
-
   _getPlaces() {
     API.getPlaces().then((response) {
       setState(() {
@@ -44,6 +43,11 @@ class _PlaceListState extends State {
     //   ),
     // );
   }
+  Future<void> _gotoLocation(double lat,double long) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,tilt: 50.0,
+      bearing: 45.0,)));
+  }
   initState() {
     super.initState();
     _getPlaces();
@@ -59,7 +63,25 @@ class _PlaceListState extends State {
       body: Stack(
         children: <Widget>[
           _googleMap(context)
+          ,_buildContainer(context)
         ],
+      )
+    );
+  }
+  Widget _buildContainer(context) {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child:Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 150.0,
+        child:ListView.builder(
+          scrollDirection:Axis.horizontal,
+          itemCount: places.length,
+          itemBuilder: (context,index) {
+            var p = places[index];
+            return _placeBox(p);
+          },
+        )
       )
     );
   }
@@ -75,6 +97,136 @@ class _PlaceListState extends State {
         },
         markers:Set<Marker>.of(_markers),
       )
+    );
+  }
+  Widget _placeBox(Place place){
+    return (
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: _boxes(
+          "https://lh5.googleusercontent.com/p/AF1QipO3VPL9m-b355xWeg4MXmOQTauFAEkavSluTtJU=w225-h160-k-no",
+          double.parse(place.lat), double.parse(place.lng),place.name??place.address),
+      ));
+  }
+  Widget _boxes(String _image, double lat,double long,String restaurantName) {
+    return  GestureDetector(
+      onTap: () {
+        _gotoLocation(lat,long);
+      },
+      child:Container(
+        child: new FittedBox(
+          child: Material(
+            color: Colors.white,
+            elevation: 14.0,
+            borderRadius: BorderRadius.circular(24.0),
+            shadowColor: Color(0x802196F3),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 180,
+                  height: 200,
+                  child: ClipRRect(
+                    borderRadius: new BorderRadius.circular(24.0),
+                    child: Image(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(_image),
+                    ),
+                  ),),
+                  Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: myDetailsContainer1(restaurantName),
+                  ),
+                ),
+              ],)
+          ),
+        ),
+      ),
+  );
+}
+  Widget myDetailsContainer1(String restaurantName) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Container(
+              child: Text(restaurantName,
+            style: TextStyle(
+                color: Color(0xff6200ee),
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold),
+          )),
+        ),
+        SizedBox(height:5.0),
+        Container(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                  child: Text(
+                "4.1",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                ),
+              )),
+              // Container(
+              //   child: Icon(
+              //     FontAwesomeIcons.solidStar,
+              //     color: Colors.amber,
+              //     size: 15.0,
+              //   ),
+              // ),
+              // Container(
+              //   child: Icon(
+              //     FontAwesomeIcons.solidStar,
+              //     color: Colors.amber,
+              //     size: 15.0,
+              //   ),
+              // ),
+              // Container(
+              //   child: Icon(
+              //     FontAwesomeIcons.solidStar,
+              //     color: Colors.amber,
+              //     size: 15.0,
+              //   ),
+              // ),
+              // Container(
+              //   child: Icon(
+              //     FontAwesomeIcons.solidStar,
+              //     color: Colors.amber,
+              //     size: 15.0,
+              //   ),
+              // ),
+              // Container(
+              //   child: Icon(
+              //     FontAwesomeIcons.solidStarHalf,
+              //     color: Colors.amber,
+              //     size: 15.0,
+              //   ),
+              // ),
+               Container(
+                  child: Text(
+                "(946)",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                ),
+              )),
+            ],
+          )),
+          SizedBox(height:5.0),
+        Container(
+            child: Text(
+          "Closed \u00B7 Opens 17:00 Thu",
+          style: TextStyle(
+              color: Colors.black54,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold),
+        )),
+      ],
     );
   }
 }
