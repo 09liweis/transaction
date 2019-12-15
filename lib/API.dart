3a65e3c7ddc149ad754e4899b8c1d2e1f7e9bcf5
Sam Li <weisen.li@hotmail.com>
2019-12-15 13:12:07 -0500

    get transactions from place detail

--- lib/API.dart
+++ lib/API.dart
@@ -2,6 +2,8 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import './models/Place.dart';
+import './models/Transaction.dart';
 
 const baseUrl = "https://samliweisen.herokuapp.com/api/";
 // const baseUrl = "http://localhost:8081/api/";
@@ -23,8 +25,21 @@ class API {
   static Future getPlaces() {
     return http.get(baseUrl+'places');
   }
-  static Future getPlace(String id) {
-    return http.get(baseUrl+'places'+id);
+  Future<Place> getPlace(String id) async {
+    var url = baseUrl+'places/'+id;
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON.
+      var placeJson = json.decode(response.body);
+      var place = Place.fromJson(placeJson);
+      Iterable transactionsJson = placeJson['transactions'];
+      List<Transaction> transactions = transactionsJson.map((model) => Transaction.fromJson(model)).toList();
+      place.transactions = transactions;
+      return place;
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
   }
   static Future upsertPlace(Map data) async {
     var url = baseUrl + 'places';
