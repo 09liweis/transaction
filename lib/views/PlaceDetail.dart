a78e790468384158579a5f93b8c9a319a260add7
Sam Li <weisen.li@hotmail.com>
2019-12-15 13:13:17 -0500

    update place detail with transactions widget

--- lib/views/PlaceDetail.dart
+++ lib/views/PlaceDetail.dart
@@ -1,15 +1,18 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../models/Place.dart';
+import '../API.dart';
 import './PlaceForm.dart';
+import '../widgets/Transactions.dart';
 class PlaceDetail extends StatelessWidget {
   final Place place;
+  API api = API();
   PlaceDetail({Key key, @required this.place}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    var t = place;
     return Scaffold(
       appBar: AppBar(
-        title: Text(t.name??t.address),
+        title: Text(place.name??place.address),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.edit),
@@ -26,10 +29,24 @@ class PlaceDetail extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(t.address),
-          ]
+        child: FutureBuilder<Place>(
+          future:api.getPlace(place.id),
+          builder: (context,snapshot) {
+            if (snapshot.hasData) {
+              Place p = snapshot.data;
+              var transactions = p.transactions;
+              return (
+                WidgetTransactions(context, transactions)
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("${snapshot.error}"),
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator()
+            );
+          },
         )
       ),
     );
