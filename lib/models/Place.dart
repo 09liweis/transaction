437f2b8a327893bf7ff2b61bfe86a893dcad5158
Sam Li <weisen.li@hotmail.com>
2019-12-14 21:39:29 -0500

    update place _id to id in model

--- lib/models/Place.dart
+++ lib/models/Place.dart
@@ -1,13 +1,13 @@
 class Place {
-  String _id;
+  String id;
   String placeId;
   String name;
   String lat;
   String lng;
   String address;
 
-  Place(String _id, String placeId, String address, String name, String lat,String lng) {
-    this._id = _id;
+  Place(String id, String placeId, String address, String name, String lat,String lng) {
+    this.id = id;
     this.placeId = placeId;
     this.name = name;
     this.lat = lat;
@@ -16,7 +16,7 @@ class Place {
   }
 
   Place.fromJson(Map json)
-      : _id = json['_id'],
+      : id = json['_id'],
         placeId = json['place_id'],
         name = json['name'],
         lat = json['lat'],
@@ -24,6 +24,6 @@ class Place {
         lng = json['lng'];
 
   Map toJson() {
-    return {'_id': _id, 'place_id':placeId, 'name': name, 'address':address, 'lat': lat,'lng':lng};
+    return {'_id': id, 'place_id':placeId, 'name': name, 'address':address, 'lat': lat,'lng':lng};
   }
 }
