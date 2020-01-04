5058cd5860d6302f41a433f6142cb8102a867ed1
Sam Li <weisen.li@hotmail.com>
2020-01-04 10:54:15 -0500

    add editTransaction on transactiondetail

--- lib/views/TransactionDetail.dart
+++ lib/views/TransactionDetail.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 import '../models/Transaction.dart';
 import '../views/TransactionForm.dart';
-class TransactionDetail extends StatelessWidget {
+class TransactionDetail extends StatefulWidget {
   final Transaction transaction;
   TransactionDetail({Key key, @required this.transaction}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    var t = transaction;
+  createState() => _TransactionDetailState();
+}
+class _TransactionDetailState extends State<TransactionDetail> {
+  Future _editTransaction(Transaction t) async {
+    var result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TransactionForm(transaction:t)),
+    );
+    if (result!=null){
+      print(result);
+      // Scaffold.of(context).showSnackBar(SnackBar(content: Text(result['msg']),duration: Duration(seconds: 3),));
+    }
+  }
+  initState() {
+    super.initState();
+  }
+
+  dispose() {
+    super.dispose();
+  }
+  @override
+  Widget build(context) {
+    var t = widget.transaction;
     return Scaffold(
       appBar: AppBar(
         title: Text(t.title),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.edit),
-            onPressed: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => TransactionForm(transaction:transaction)
-                ),
-              );
+            onPressed: () {
+              _editTransaction(t);
             },
           )
         ]
